f0c42ad21f397fc61b3d5d2f4783f224e3ae20e3
choyeonjae <chojudy2@naver.com>
2017-05-22 20:13:56 +0900

    Create minority.c

---
+++ minority.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int prime_check(int n);
+
+int main(void) 
+{
+	int a;
+	printf("양수입력 : ");
+	scanf("%d", &a);
+
+	int prime;
+	prime = prime_check(a);
+}
+
+int prime_check(int n) 
+{
+	int i;
+	for (i = 0; i < n; i++) 
+	{
+		
+	}
+}
